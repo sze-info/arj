09f956aa82733bb631efda7dc4e31aaaed4d0c45
horverno <horverno@gmail.com>
2023-07-14 15:43:26 +0200

    Géptermi telepítés v2 gazebo

--- docs/telepites/install_humble.sh
+++ docs/telepites/install_humble.sh
@@ -54,5 +54,17 @@ cd ~/ros2_ws
 colcon build
 
 echo ""
+echo "++++ install gazebo ignition fortress ++++"
+echo ""
+
+sudo apt-get update -y
+sudo apt-get install lsb-release wget gnupg -y
+
+sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install ignition-fortress -y
+
+echo ""
 echo "++++ install script end ++++"
 echo ""
